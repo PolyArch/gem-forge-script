db2e4e04dd002d7cd67f1b95d1b1d9289187e9b6
Zhengrong Wang <seanyukigeek@gmail.com>
2021-03-16 11:31:33 -0700

    [misc] Print command in one line.

--- Util.py
+++ Util.py
@@ -8,6 +8,7 @@ def call_helper(cmd, stdout=None):
     Helper function to call a command and print the actual command when failed.
     """
     print(' \\\n'.join(cmd))
+    print('Executing \n{cmd}'.format(cmd=' '.join(cmd)))
     try:
         subprocess.check_call(cmd, stdout=stdout)
     except subprocess.CalledProcessError as e:
